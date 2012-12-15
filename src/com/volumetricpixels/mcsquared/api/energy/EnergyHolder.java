cee00dc1d1ad3978ac23c623387d501e083b23c5
James Hutchison <th3hutch@yahoo.co.uk>
2012-12-15 00:29:38 +0000

    Added add/remove methods for energy and maximum and stored energy

--- src/com/volumetricpixels/mcsquared/api/energy/EnergyHolder.java
+++ src/com/volumetricpixels/mcsquared/api/energy/EnergyHolder.java
@@ -6,6 +6,7 @@ package com.volumetricpixels.mcsquared.api.energy;
 public abstract class EnergyHolder implements EnergyTransferer {
 
     private double maximum_energy;
+    private double energy_stored;
 
     public EnergyHolder(double maximum_energy) {
         this.maximum_energy = maximum_energy;
@@ -19,6 +20,33 @@ public abstract class EnergyHolder implements EnergyTransferer {
         this.maximum_energy=maximum_energy;
     }
     
-    public abstract void onEnergyAccept(EnergySource source);
+    public double getEnergyStored() {
+        return energy_stored;
+    }
     
+    public void addEnergy(double energy) {
+        if (energy_stored + energy <= maximum_energy && energy > 0) {
+            energy_stored += energy;
+        }
+    }
+    
+    public void removeEnergy(double energy) {
+        if (energy_stored - energy >= 0 && energy > 0) {
+            energy_stored -= energy;
+        }
+    }
+    
+    @Override
+    public void transferTo(EnergyHolder destination, double energy) {
+        destination.onEnergyReceive(energy);
+    }
+    
+    @Override
+    public void onEnergyReceive(double energy) {
+        if (energy + getEnergyStored() > this.getMaximum_energy()) {
+            //Do stuff for wire is full
+        } else {
+            addEnergy(energy);
+        }
+    }
 }
