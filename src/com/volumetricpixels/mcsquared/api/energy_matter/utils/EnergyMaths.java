11c06fb8b5faf632ff8ab10fe3eed61ad5bed08d
James Hutchison <th3hutch@yahoo.co.uk>
2012-12-15 21:53:09 +0000

    Added unit name

--- src/com/volumetricpixels/mcsquared/api/energy_matter/utils/EnergyMaths.java
+++ src/com/volumetricpixels/mcsquared/api/energy_matter/utils/EnergyMaths.java
@@ -7,8 +7,9 @@ import com.volumetricpixels.mcsquared.api.energy_matter.EnergyPrefix;
  */
 public class EnergyMaths {
     
-    private static final float ENERGY_CONSTANT = 2.5f;
-    private static final float CONVERT_RATE = 0.95f;
+    public static final String UNIT = "Joules";
+    public static final float ENERGY_CONSTANT = 2.5f;
+    public static final float CONVERT_RATE = 0.95f;
 
     private EnergyMaths(){
     }
