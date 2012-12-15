1d399e7831a8563c37cb3336de00fad352f90915
James Hutchison <th3hutch@yahoo.co.uk>
2012-12-15 19:33:47 +0000

    EnergyReceiver Interface used for blocks which can receive energy

---
+++ src/com/volumetricpixels/mcsquared/api/energy_matter/EnergyReceiver.java
@@ -0,0 +1,15 @@
+package com.volumetricpixels.mcsquared.api.energy_matter;
+
+/**
+ * @author thehutch
+ */
+public interface EnergyReceiver extends Connectable {
+    
+    /**
+     * Called when energy is received from a source
+     * 
+     * @param energy 
+     */
+    public void onEnergyReceive(double energy);
+
+}
