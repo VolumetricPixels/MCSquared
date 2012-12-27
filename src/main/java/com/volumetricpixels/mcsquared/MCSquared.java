f63801cfa10f9efcd0df70cf0dc4ad33f1275e65
James Hutchison <th3hutch@yahoo.co.uk>
2012-12-27 15:33:18 +0000

    Minor changes to main class

--- src/main/java/com/volumetricpixels/mcsquared/MCSquared.java
+++ src/main/java/com/volumetricpixels/mcsquared/MCSquared.java
@@ -1,15 +1,15 @@
 package com.volumetricpixels.mcsquared;
 
+import java.util.logging.Level;
 import org.spout.api.plugin.CommonPlugin;
 
 public class MCSquared extends CommonPlugin {
 
+    public void onEnable() {
+		getLogger().log(Level.INFO, "MCSquared version: {0} has been Enabled.", getDescription().getVersion());
+	}
+    
 	public void onDisable() {
-		getLogger().info("MCSquared Disabled.");
+		getLogger().log(Level.INFO, "MCSquared version: {0} has been Disabled.", getDescription().getVersion());
 	}
-
-	public void onEnable() {
-		getLogger().info("MCSquared Enabled.");
-	}
-
 }
