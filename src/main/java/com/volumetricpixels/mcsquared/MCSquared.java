28caaf01c9d0361fe8bbb914189008d220d90976
Ollie <dzineit@live.co.uk>
2012-12-30 16:54:01 +0000

    Largely improved energy system and make it easier to work with
    Signed-off-by: Ollie <dzineit@live.co.uk>

--- src/main/java/com/volumetricpixels/mcsquared/MCSquared.java
+++ src/main/java/com/volumetricpixels/mcsquared/MCSquared.java
@@ -1,15 +1,20 @@
 package com.volumetricpixels.mcsquared;
 
 import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.spout.api.plugin.CommonPlugin;
 
 public class MCSquared extends CommonPlugin {
+    private Logger logger;
 
     public void onEnable() {
-		getLogger().log(Level.INFO, "MCSquared version: {0} has been Enabled.", getDescription().getVersion());
-	}
-    
-	public void onDisable() {
-		getLogger().log(Level.INFO, "MCSquared version: {0} has been Disabled.", getDescription().getVersion());
-	}
+        logger = getLogger();
+
+        logger.log(Level.INFO, "MCSquared version {0} is now enabled", getDescription().getVersion());
+    }
+
+    public void onDisable() {
+        logger.log(Level.INFO, "MCSquared version {0} is now disabled", getDescription().getVersion());
+    }
 }
