aa7f5a291121c6bd511396a6f39a305d13cdb799
thehutch <>
2013-01-01 23:15:59 +0000

    Added EnergyEvent and EnergyCarrierOverload and implemented into EnergyCarrier Signed-off-by:thehutch

--- src/main/java/com/volumetricpixels/mcsquared/api/energy/EnergyCarrier.java
+++ src/main/java/com/volumetricpixels/mcsquared/api/energy/EnergyCarrier.java
@@ -1,24 +1,30 @@
 package com.volumetricpixels.mcsquared.api.energy;
 
+import com.volumetricpixels.mcsquared.api.energy.event.EnergyCarrierOverloadEvent;
 import com.volumetricpixels.mcsquared.api.energy.impl.EnergyNodeImpl;
 import com.volumetricpixels.mcsquared.api.utils.EnergyUtils;
 import java.util.HashSet;
 import java.util.Set;
+import org.spout.api.Spout;
 import org.spout.api.material.BlockMaterial;
 
 public class EnergyCarrier extends EnergyNodeImpl implements EnergyReceiver, EnergySource {
 
     private final Set<EnergyReceiver> receivers = new HashSet<EnergyReceiver>();
     private Energy maxEnergyTransfer;
-    
+
     public EnergyCarrier() {
         maxEnergyTransfer = new Energy(100);
     }
-    
+
     @Override
     public Energy onReceive(EnergySource source, Set<EnergyNode> visited, Energy energy) {
         if (maxEnergyTransfer.compareTo(energy) > 0) {
-            getPosition().getBlock().setMaterial(BlockMaterial.AIR);
+            EnergyCarrierOverloadEvent event = new EnergyCarrierOverloadEvent(energy, this);
+            Spout.getEventManager().callEvent(event);
+            if (!event.isCancelled()) {
+                getPosition().getBlock().setMaterial(BlockMaterial.AIR);
+            }
             return energy;
         }
         visited.add(this);
@@ -34,11 +40,11 @@ public class EnergyCarrier extends EnergyNodeImpl implements EnergyReceiver, Ene
     public void removeReceiver(EnergyReceiver receiver) {
         receivers.remove(receiver);
     }
-    
+
     public Energy getMaxEnergyTransfer() {
         return maxEnergyTransfer;
     }
-    
+
     public void setMaxEnergyTransfer(Energy maximum) {
         this.maxEnergyTransfer = maximum;
     }
---
+++ src/main/java/com/volumetricpixels/mcsquared/api/energy/event/EnergyCarrierOverloadEvent.java
@@ -0,0 +1,28 @@
+package com.volumetricpixels.mcsquared.api.energy.event;
+
+import com.volumetricpixels.mcsquared.api.energy.Energy;
+import com.volumetricpixels.mcsquared.api.energy.EnergyCarrier;
+import org.spout.api.event.HandlerList;
+
+/**
+ * @author thehutch <th3hutch@yahoo.co.uk>
+ */
+public class EnergyCarrierOverloadEvent extends EnergyEvent {
+
+    private static HandlerList handlerList = new HandlerList();
+    private final EnergyCarrier carrier;
+
+    public EnergyCarrierOverloadEvent(Energy energy, EnergyCarrier wire) {
+        super(energy);
+        this.carrier = wire;
+    }
+
+    public EnergyCarrier getCarrier() {
+        return carrier;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+}
