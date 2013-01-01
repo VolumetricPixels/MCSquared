915830e2b38a5e8aaa61930955ad28156aa84416
thehutch <>
2013-01-01 22:36:55 +0000

    Added a generic energy carrier eg: Wires Signed-off-by:thehutch

---
+++ src/main/java/com/volumetricpixels/mcsquared/api/energy/EnergyCarrier.java
@@ -0,0 +1,45 @@
+package com.volumetricpixels.mcsquared.api.energy;
+
+import com.volumetricpixels.mcsquared.api.energy.impl.EnergyNodeImpl;
+import com.volumetricpixels.mcsquared.api.utils.EnergyUtils;
+import java.util.HashSet;
+import java.util.Set;
+import org.spout.api.material.BlockMaterial;
+
+public class EnergyCarrier extends EnergyNodeImpl implements EnergyReceiver, EnergySource {
+
+    private final Set<EnergyReceiver> receivers = new HashSet<EnergyReceiver>();
+    private Energy maxEnergyTransfer;
+    
+    public EnergyCarrier() {
+        maxEnergyTransfer = new Energy(100);
+    }
+    
+    @Override
+    public Energy onReceive(EnergySource source, Set<EnergyNode> visited, Energy energy) {
+        if (maxEnergyTransfer.compareTo(energy) > 0) {
+            getPosition().getBlock().setMaterial(BlockMaterial.AIR);
+            return energy;
+        }
+        visited.add(this);
+        return EnergyUtils.safeSplit(source, energy, visited, receivers);
+    }
+
+    @Override
+    public void addReceiver(EnergyReceiver receiver) {
+        receivers.add(receiver);
+    }
+
+    @Override
+    public void removeReceiver(EnergyReceiver receiver) {
+        receivers.remove(receiver);
+    }
+    
+    public Energy getMaxEnergyTransfer() {
+        return maxEnergyTransfer;
+    }
+    
+    public void setMaxEnergyTransfer(Energy maximum) {
+        this.maxEnergyTransfer = maximum;
+    }
+}
